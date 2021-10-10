c9de8612bc0415745ba3929dc4d35802bdce8854
EddyGiusepe <eddychirinos.unac@gmail.com>
2021-10-10 02:25:32 -0300

    primeiro exemplo

--- regex_1.py
+++ regex_1.py
@@ -1,6 +1,17 @@
+# O link REGEX é: https://www.youtube.com/watch?v=Jao4VwJCByk&t=135s
+# Regular Expression HOWTO: https://docs.python.org/3/howto/regex.html
+
 # Importando a biblioteca
 import re
 
-# Caracteres:
-# 
+
+print("#######################")
+print("# CARACTERES NO REGEX #")
+print("#######################")
+# . - Entende qualquer valor exceto uma nova linha
+# \. - Para buscar o caracter "."
+texto = 'arbra'
+t = re.compile('ar.ra') # vai procurar:começa com ar "tanto faz" termina com ra
+check = t.findall(texto) # Este vai nosso padrão de busca
+print(check)
 
